b5591233b0ad0ebca50a836524c87e778aea3f1b
regtrey <manigosreggie@gmail.com>
2024-02-02 21:11:02 +0800

    Implement error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import Error from './ui/Error.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<Error />}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
